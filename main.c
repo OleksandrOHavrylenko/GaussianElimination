d964ae71507c125565e25bf5e55975a29aed0b26
Oleksandr.Gavrylenko <gavrylenko.aleksandr@gmail.com>
2020-02-25 22:54:52 +0200

    Dynamic initialization

--- main.c
+++ main.c
@@ -1,73 +1,121 @@
 #include<stdio.h>
+#include<stdlib.h>
 //using namespace std;
+float** createCoefficientMatrix(int);
+void initCoefficientMatrix(float**, int);
+void printCoefficientMatrix(float**, int);
+
+void printEchelonForm(float**, const float *, int);
+void printResult(const float *, int n);
+
 int main()
 {
-    int i, j, k, n;
-    float MatA[100][100], MatB[100], X[100];
-    float Divisor, Factor, sum;
+    int  n;
+    float** MatA;
+    float* MatB;
+    float* X;
+    float divider, factor, sum;
+    printf("Enter the number of unknown variables: ");
     scanf("%d", &n);
-//    cin >> n;
+    MatA = createCoefficientMatrix(n);
+    initCoefficientMatrix(MatA, n);
+    printCoefficientMatrix(MatA, n);
+    MatB = (float*) malloc(n * sizeof(float));
+    X = (float*) malloc(n * sizeof(float));
+
+
 //reading matrix A
-    for(i=0; i< n; i++) {
-        for(j=0; j < n; j++) {
-            printf("MatA[%d][%d]", i, j);
-            scanf("%f", &MatA[i][j]);
-//            cin >> MatA[i][j];
-        }
-    }
+//    for(i=0; i< n; i++) {
+//        for(j=0; j < n; j++) {
+//            printf("MatA[%d][%d]", i, j);
+//            scanf("%f", &MatA[i][j]);
+////            cin >> MatA[i][j];
+//        }
+//    }
 //reading matrix B
-    for(i=0; i< n; i++) {
+    for(int i=0; i< n; i++) {
         printf("MatB[%d]", i);
         scanf("%f", &MatB[i]);
 //        cin >> MatB[i];
     }
+    printEchelonForm(MatA, MatB, n);
 //Gauss elimination
-    for (i=0; i< n; i++) {
-        Divisor = MatA[i][i];
-        MatA[i][i] = 1.0;
+    for (int i=0; i< n; i++) {
+        divider = MatA[i][i];
+        MatA[i][i] = 1.0f;
 // divide all values in the row by the divisor
 // to recalculate all coefficients in that row
-        for (j = i+1; j < n; j++) {
-            MatA[i][j] = MatA[i][j]/Divisor;
+        for (int j = i+1; j < n; j++) {
+            MatA[i][j] = MatA[i][j] / divider;
         }
 //Also divide the corresponding RHS element
-        MatB[i] = MatB[i]/Divisor;
+        MatB[i] = MatB[i] / divider;
 // now replace subsequent rows, by subtracting the
 // appropriate portion of the ith equation from it
         if (i+1 < n) {
-            for (k=i+1; k<n; k++) {
-                Factor = MatA[k][i];
-                MatA[k][i] = 0.0;
-                for (j = i+1; j < n; j++) {
-                    MatA[k][j] = MatA[k][j] - Factor * MatA[i][j];
+            for (int k=i+1; k<n; k++) {
+                factor = MatA[k][i];
+                MatA[k][i] = 0.0f;
+                for (int j = i+1; j < n; j++) {
+                    MatA[k][j] = MatA[k][j] - factor * MatA[i][j];
                 }
-                MatB[k] = MatB[k] - Factor * MatB[i];
+                MatB[k] = MatB[k] - factor * MatB[i];
             }
         }
     }
 // back substitution starting with last variable
     X[n-1] = MatB[n-1];
-    for (i = n-2; i>=0; i--) {
+    for (int i = n-2; i>=0; i--) {
 // Sum up ith row using values of X already determined
         sum = 0.0;
-        for (j = i+1; j < n; j++) {
+        for (int j = i+1; j < n; j++) {
             sum = sum + MatA[i][j] * X[j];
         }
         X[i] = MatB[i] - sum;
     }
-//output the results
-    for(i=0; i< n; i++) {
-        for (j = 0; j < n; j++) {
-            printf("%f ", MatA[i][j]);
-//            cout << MatA[i][j] << " ";
-        }
-        printf("%f\n", MatB[i]);
-//        cout << " " << MatB[i] << endl;
-    }
-    for (i=0; i<n; i++) {
-        printf("X[%d] is: %f", i, X[i]);
-//        cout << "X[" << i << "] is: " ;
-//        cout << X[i] << endl;
-    }
+
+    printEchelonForm(MatA, MatB, n);
+    printResult(X, n);
     return 0;
+}
+
+void printEchelonForm(float** M, const float *RHS, int size) {
+    for(int i=0; i< size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%5.2f ", M[i][j]);
+        }
+        printf("%10.2f\n", RHS[i]);
+    }
+}
+
+void printResult(const float *X, int n) {
+    for (int i=0; i < n; i++) {
+        printf("X[%d] = %5.2f;\n", i, X[i]);
+    }
+}
+
+float** createCoefficientMatrix(int n) {
+    float** matrix =(float**) malloc(n * sizeof(float*));
+    for(int i = 0; i < n; i++) {
+        matrix[i] = (float *) malloc(n * sizeof(float));
+    }
+    return matrix;
+}
+
+void initCoefficientMatrix(float** M, int size) {
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            M[i][j] = i + j +1;
+        }
+    }
+}
+
+void printCoefficientMatrix(float** M, int size) {
+    printf("\n");
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            printf("%5.2f", M[i][j]);
+        }
+        printf("\n");
+    }
 }
